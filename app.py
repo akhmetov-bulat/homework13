92de624074a8a99553d866d945676f1d884c8b15
Bulat-Akhmetov <akhmetov.bulat@gmail.com>
2021-11-03 23:09:11 -0500

    created adding a book

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 import json
 from flask import Flask, request, Response
+from random import randint
 
 app = Flask(__name__)
 
 
 def read_books_json():
-    with open('books.json',"r", encoding='utf-8') as f:
+    with open('books.json', "r", encoding='utf-8') as f:
         file = f.read()
         if file:
             books = json.loads(file)
             return books
         return []
 
-def write_json(books):
-    pass
+
+def new_isbn():
+    books = read_books_json()
+    unique_isbn = False
+    while not unique_isbn:
+        isbn_list = []
+        isbn_list.append(str(randint(100, 999)))
+        isbn_list.append(str(randint(0, 9)))
+        isbn_list.append(str(randint(100, 999)))
+        isbn_list.append('0' + str(randint(1000, 9999)))
+        isbn_list.append(str(randint(0, 9)))
+        isbn = "-".join(isbn_list)
+        for book in books:
+            if book['isbn'] == isbn:
+                continue
+        unique_isbn = True
+        return isbn
 
 
 @app.route('/books/<int:book_id>')
@@ -22,29 +38,27 @@ def get_book_by_id(book_id: int):
     if books:
         for book in books:
             if book['id'] == book_id:
-                return Response(json.dumps(book, ensure_ascii = False), content_type='application/json', status=400)
+                return Response(json.dumps(book, ensure_ascii=False), content_type='application/json', status=400)
         return Response("there is no book on this id", content_type="text/html", status=404)
     return Response("bookshelf is clear", content_type='text/html', status=404)
 
 
-@app.route('/books/add', methods = ['POST'])
+@app.route('/books/add', methods=['PUT'])
 def add_book():
     books = read_books_json()
-    def new_id():
+    def new_id():  # В роут создания книги добавьте генерацию id
         max_id = 0
         for book in books:
             if book['id'] > max_id:
                 max_id = book['id']
-            return max_id + 1
-    book = request.get_json()
-    book['id'] = new_id()
-    book['ISBN'] = new_isbn()
-    books.append(book)
+        return max_id + 1
+    new_book = request.get_json()
+    new_book['id'] = new_id()
+    new_book['isbn'] = new_isbn()
+    books.append(new_book)
     with open('books.json', 'w', encoding='utf-8',) as f:
-        json.dump(books, f, ensure_ascii=False,indent='\t')
+        json.dump(books, f, ensure_ascii=False, indent='\t')
     return Response(json.dumps(books, ensure_ascii=False), content_type='application/json', status=201)
-
-
 
 
 if __name__ == '__main__':
